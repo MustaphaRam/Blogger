c16108b162f7e75db876c2076333f573a871910a
MustaphaRam <mustapha.mad.75@gmail.com>
2023-11-27 07:12:30 +0100

    first commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    /* private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    } */
+
+    public function __construct( 
+        private PasswordHasherFactoryInterface $passwordHasherFactory,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        
+        // Create an admin user
+        $admin = new User();
+        $admin->setName('admin');
+        $admin->setLastname('admin');
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasherFactory->getPasswordHasher(User::class)->hash('admin123'));
+        /* $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'admin123' // Change this to a secure password
+        )); */
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
